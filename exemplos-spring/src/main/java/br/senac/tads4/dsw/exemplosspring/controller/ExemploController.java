22abc2f866e88c86c21d9fd447b68af688122a92
Fernando Tsuda <ftsuda.senac@gmail.com>
2018-04-25 21:58:58 -0300

    Listagem por categoria e paginacao

--- exemplos-spring/src/main/java/br/senac/tads4/dsw/exemplosspring/controller/ExemploController.java
+++ exemplos-spring/src/main/java/br/senac/tads4/dsw/exemplosspring/controller/ExemploController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -43,6 +44,7 @@ public class ExemploController {
   }
   
   @GetMapping("/ex3")
+  @RequestMapping(value = "/exemplo", method = RequestMethod.GET)
   public String executar3(@RequestParam(value = "nome", required = false) String nomeParam, 
           Model modelo) {
     modelo.addAttribute("nomeInformado", nomeParam);
